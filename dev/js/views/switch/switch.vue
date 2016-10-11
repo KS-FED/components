83f7fdb70987091c3c9b948d76779fa2353abf4b
Zhangjie <Zhangjie@kashuo.com>
2016-10-11 17:04:36 +0800

    IosSwitch 组件：更新有关于 change 事件的描述

--- dev/js/views/switch/switch.vue
+++ dev/js/views/switch/switch.vue
@@ -163,7 +163,7 @@
           <tbody>
           <tr style="text-align: left">
             <td>onChange</td>
-            <td>在状态切换时会触发的事件</td>
+            <td>在状态切换时会触发的事件, 该事件会携带当前的 Switch 状态传递给处理函数</td>
             <td>event</td>
             <td>---</td>
             <td>---</td>
