c47b331bad12b6702b335afdf092fa25bfd7f894
刘丽辉 <liulihui@kashuo.com>
2016-10-21 16:10:32 +0800

    layout   调整

--- dev/js/views/date/date.vue
+++ dev/js/views/date/date.vue
@@ -1,6 +1,76 @@
 <template>
 	<div>
-		<div class="date" style="margin-bottom: 400px;">
+		<!-- 只有日期 -->
+		<!-- <div class="picker-wrap">
+			<div class="date-head">
+				<div class="retreat">&lt;</div>
+				<div class="year">2016年</div>
+				<div class="interstice"></div>
+				<div class="month">4月</div>
+				<div class="next">&gt;</div>
+			</div>
+			<div class="date-week">
+				<span>一</span>
+				<span>二</span>
+				<span>三</span>
+				<span>四</span>
+				<span>五</span>
+				<span class="week">六</span>
+				<span class="week">日</span>
+			</div>
+			<div class="date-days">
+				<span class="pass">1</span>
+				<span class="pass">2</span>
+				<span class="pass">3</span>
+				<span class="pass">4</span>
+				<span>5</span>
+				<span>6</span>
+				<span>7</span>
+			</div>
+			<div class="date-days">
+				<span>8</span>
+				<span>9</span>
+				<span>10</span>
+				<span>11</span>
+				<span>12</span>
+				<span>13</span>
+				<span>14</span>
+			</div>
+			<div class="date-days">
+				<span>15</span>
+				<span>16</span>
+				<span class="active">17</span>
+				<span>18</span>
+				<span>19</span>
+				<span>20</span>
+				<span>21</span>
+			</div>
+			<div class="date-days">
+				<span>22</span>
+				<span>23</span>
+				<span>24</span>
+				<span>25</span>
+				<span>26</span>
+				<span>27</span>
+				<span>28</span>
+			</div>
+			<div class="date-days">
+				<span>29</span>
+				<span>30</span>
+				<span class="future">1</span>
+				<span class="future">2</span>
+				<span class="future">3</span>
+				<span class="future">4</span>
+				<span class="future">5</span>
+			</div>
+			<div class="date-btn">
+				<span class="today">今天</span>
+				<span class="clear">清除</span>
+			</div>
+		</div> -->
+
+		<!-- 日期默认样式——周 -->
+		<!-- <div class="date" style="margin-bottom: 400px;">
 			<div class="date-input">
 				<div class="col-auto"><i class="icon">&#xe615;</i></div>
 				<input type="text" class="col">
@@ -72,7 +142,85 @@
 					<span class="clear">清除</span>
 				</div>
 			</div>
+		</div> -->
+
+		<!-- 日期默认样式——月 -->
+		<div class="date-month" style="margin-bottom: 400px;">
+			<div class="date-input">
+				<div class="col-auto"><i class="icon">&#xe615;</i></div>
+				<input type="text" class="col">
+			</div>
+			<div class="picker-wrap">
+				<div class="date-head">
+					<div class="retreat">&lt;</div>
+					<div class="year">2016年</div>
+					<div class="next">&gt;</div>
+				</div>
+				<div class="date-week">
+					<span>一</span>
+					<span>二</span>
+					<span>三</span>
+					<span>四</span>
+					<span>五</span>
+					<span class="week">六</span>
+					<span class="week">日</span>
+				</div>
+				<div class="date-days">
+					<span class="pass">1</span>
+					<span class="pass">2</span>
+					<span class="pass">3</span>
+					<span class="pass">4</span>
+					<span>5</span>
+					<span>6</span>
+					<span>7</span>
+				</div>
+				<div class="date-days">
+					<span>8</span>
+					<span>9</span>
+					<span>10</span>
+					<span>11</span>
+					<span>12</span>
+					<span>13</span>
+					<span>14</span>
+				</div>
+				<div class="date-days">
+					<span>15</span>
+					<span>16</span>
+					<span class="active">17</span>
+					<span>18</span>
+					<span>19</span>
+					<span>20</span>
+					<span>21</span>
+				</div>
+				<div class="date-days">
+					<span>22</span>
+					<span>23</span>
+					<span>24</span>
+					<span>25</span>
+					<span>26</span>
+					<span>27</span>
+					<span>28</span>
+				</div>
+				<div class="date-days">
+					<span>29</span>
+					<span>30</span>
+					<span class="future">1</span>
+					<span class="future">2</span>
+					<span class="future">3</span>
+					<span class="future">4</span>
+					<span class="future">5</span>
+				</div>
+				<div class="date-btn">
+					<span class="today">今天</span>
+					<span class="clear">清除</span>
+				</div>
+			</div>
 		</div>
+
+
+
+
+
 
 		<pre>
         <code class="html">
