46747b557ca86351533068cfabb1d563010be76c
zdz <1696498456@qq.com>
2016-10-31 13:50:01 +0800

    ~

--- dev/js/views/validation/validation.vue
+++ dev/js/views/validation/validation.vue
@@ -2,35 +2,22 @@
   <div style="margin: 100px;">
 
 <div id="validation-container">
-  <p>
-    rd1: username 必须输入
-    rd2: username tow 都必须输入
-  </p>
-  <validator name="validation" :groups="['child1', 'child2']">
+  <validator name="validation" :groups="['child1', 'child2','common']">
+
     <form novalidate>
+        <input type="text" name="" value="" v-validate:a="['required']" group="common" >
+        <input type="text" name="" value="" v-validate:b="['required']" group="common" >
       <div class="radio-field">
         <label for="rd1">rd1:</label>
-        <input id="rd1" type="radio" value="child1" group="child1" v-model="radio" v-validate:condition1="['required']">
+        <input id="rd1" type="radio" value="child1" v-model="radio">
         <label for="rd2">rd2:</label>
-        <input id="rd2" type="radio" value="child2" group="child2" v-model="radio" v-validate:condition2="['required']">
+        <input id="rd2" type="radio" value="child2" v-model="radio">
       </div>
       <div class="username-field">
         <label for="username">username:</label>
         <input id="username" type="text" group="child1" v-validate:username="['required']">
       </div>
-      <div class="username-field">
-        <label for="tow">tow:</label>
-        <input id="tow" type="text" group="child2" v-validate:tow="['required']">
-      </div>
-      <div class="comment-field">
-        <label for="comment">comment:</label>
-        <input id="comment" type="text" v-validate:comment="{ maxlength: 256 }">
-      </div>
-      <div class="errors">
-        <p v-if="$validation.username.required">Required your name.</p>
-        <p v-if="$validation.comment.maxlength">Your comment is too long.</p>
-      </div>
-      <input type="submit" value="send" v-if="$validation[radio].valid">
+      <input type="button" value="send" v-on:click="submit()">
     </form>
   </validator>
 </div>
@@ -46,7 +33,21 @@
       }
     },
 
+    watch: {
+      radio () {
+        console.log(this.$validation)
+      }
+    },
+
     methods: {
+      submit(){
+            // ($validation[radio].valid)
+            console.log(this.$validation[this.radio].valid)
+            console.log(this.$validation['common'].valid)
+            console.log(this.$validation.valid && this.$validation[this.radio].valid)
+
+            // console.log(this.$validation.valid)
+      }
     }
   }
 </script>
