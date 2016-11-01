06c0867887e0c43370c96255d76cbeb51115d60d
刘丽辉 <liulihui@kashuo.com>
2016-11-01 14:15:36 +0800

    省市区三级联动

---
+++ dev/js/views/cityselect/city-select.vue
@@ -0,0 +1,55 @@
+<template>
+	<div class="city-select">
+		<div class="city-input">
+			<div class="col-auto date-icon"><i class="icon">&#xe668;</i></div>
+			<input type="text" class="col">
+		</div>
+		<div class="city-bd">
+			<ul class="tab ib-w">
+			    <li class="active ib">省</li>
+			    <li class="line"><a>|</a></li>
+			    <li class="ib">市</li>
+			    <li class="line"><a>|</a></li>
+			    <li class="ib">区/县</li>
+			</ul>
+			<table class="list">
+				<tr>
+					<td>北京</td>
+					<td>天津</td>
+					<td class="active">河北</td>
+					<td>安徽</td>
+				</tr>
+				<tr>
+					<td>山西</td>
+					<td>辽宁</td>
+					<td>新疆</td>
+					<td>山东</td>
+				</tr>
+				<tr>
+					<td>北京</td>
+					<td>天津</td>
+					<td>河北</td>
+					<td>安徽</td>
+				</tr>
+				<tr>
+					<td>山西</td>
+					<td>辽宁</td>
+					<td>吉林</td>
+					<td>山东</td>
+				</tr>
+				<tr>
+					<td>北京</td>
+					<td>天津</td>
+					<td>河北</td>
+					<td>安徽</td>
+				</tr>
+				<tr>
+					<td>山西</td>
+					<td>辽宁</td>
+					<td>吉林</td>
+					<td>山东</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+</template>
