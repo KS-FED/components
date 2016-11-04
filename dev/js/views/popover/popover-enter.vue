e4b58bb48d651247e171851d0a21b67d07510d14
刘丽辉 <liulihui@kashuo.com>
2016-11-04 13:51:09 +0800

    样式调整

--- dev/js/views/popover/popover-enter.vue
+++ dev/js/views/popover/popover-enter.vue
@@ -1,15 +1,50 @@
 <template>
-	<div style="margin: 100px;">
+	<div>
 		<!-- 弹出框-输入 -->
-		<div class="popover-enter-top">
+		<div class="Ks-popover-enter">
 			<h2>标题</h2>
-			<div class="enter-input">
-				<div class="col"><input type="text" class="input"></div>
-				<div class="col-auto"><button class="btn-primary"><i class="icon">&#xe614;</i></button></div>
-				<div class="col-auto"><button class="btn-plain-dark"><i class="icon">&#xe611;</i></button></div>
+			<div class="Ks-popover-enter__input">
+				<div class="col">
+					<input type="text" class="input">
+				</div>
+				<div class="col-auto">
+					<button class="btn-primary Ks-popover-enter__btn-primary">
+						<i class="icon">&#xe614;</i>
+					</button>
+				</div>
+				<div class="col-auto">
+					<button class="btn-plain-dark">
+						<i class="icon">&#xe611;</i>
+					</button>
+				</div>
 			</div>
 			<p>这是帮助信息</p>
 			<span></span>
 		</div>
+
+		<pre>
+			<code class="html">
+			<div class="Ks-popover-enter">
+				<h2>标题</h2>
+				<div class="Ks-popover-enter__input">
+					<div class="col">
+						<input type="text" class="input">
+					</div>
+					<div class="col-auto">
+						<button class="btn-primary Ks-popover-enter__btn-primary">
+							<i class="icon">&#xe614;</i>
+						</button>
+					</div>
+					<div class="col-auto">
+						<button class="btn-plain-dark">
+							<i class="icon">&#xe611;</i>
+						</button>
+					</div>
+				</div>
+				<p>这是帮助信息</p>
+				<span></span>
+			</div>	
+			</code>
+		</pre>
 	</div>
 </template>
