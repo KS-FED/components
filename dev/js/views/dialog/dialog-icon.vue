5547dcad23abfb3f9e731b592f2bce180ba2675f
刘丽辉 <liulihui@kashuo.com>
2016-11-07 14:19:03 +0800

    dialog-icon

--- dev/js/views/dialog/dialog-icon.vue
+++ dev/js/views/dialog/dialog-icon.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 
-    <div class="playground">
+    <!-- <div class="playground">
       <ks-dialog-entity :mask="false">
         <span slot="title">测试1 --- mask false</span>
         <p slot="text" style="margin: 0">
@@ -10,64 +10,126 @@
           测试内容测试内容测试内容测试内容测试内
         </p>
       </ks-dialog-entity>
-    </div>
+    </div> -->
 
 		<!-- 红色icon弹出框 -->
-		<div class="dialog-icon-danger">
-			<div class="icon-bor"><i class="icon">&#xe60d;</i></div>
-			<div class="title">
+		<div class="KsDialogIcon KsDialogIcon--danger">
+			<div class="KsDialogIcon__icon-bor"><i class="icon">&#xe60d;</i></div>
+			<div class="KsDialogIcon__title">
 				<h2>标题</h2>
 			</div>
-			<div class="content">
+			<div class="KsDialogIcon__content">
 				警告信息！警告信息！警告信息！
 			</div>
-			<div class="operation">
+			<div class="KsDialogIcon__operation">
 				<button class="btn-plain-dark">取消</button>
 				<button class="btn-danger">确定</button>
 			</div>
 		</div>
 		<!-- 青色icon弹出框 -->
-		<div class="dialog-icon-info">
-			<div class="icon-bor"><i class="icon">&#xe61b;</i></div>
-			<div class="title">
+		<div class="KsDialogIcon KsDialogIcon--info">
+			<div class="KsDialogIcon__icon-bor"><i class="icon">&#xe61b;</i></div>
+			<div class="KsDialogIcon__title">
 				<h2>标题</h2>
 			</div>
-			<div class="content">
+			<div class="KsDialogIcon__content">
 				警告信息！警告信息！警告信息！
 			</div>
-			<div class="operation">
+			<div class="KsDialogIcon__operation">
 				<button class="btn-plain-dark">取消</button>
 				<button class="btn-info">确定</button>
 			</div>
 		</div>
 		<!-- 橙色icon弹出框 -->
-		<div class="dialog-icon-warning">
-			<div class="icon-bor"><i class="icon">&#xe60d;</i></div>
-			<div class="title">
+		<div class="KsDialogIcon KsDialogIcon--warning">
+			<div class="KsDialogIcon__icon-bor"><i class="icon">&#xe60d;</i></div>
+			<div class="KsDialogIcon__title">
 				<h2>标题</h2>
 			</div>
-			<div class="content">
+			<div class="KsDialogIcon__content">
 				警告信息！警告信息！警告信息！
 			</div>
-			<div class="operation">
+			<div class="KsDialogIcon__operation">
 				<button class="btn-plain-dark">取消</button>
 				<button class="btn-warning">确定</button>
 			</div>
 		</div>
 
 		<!-- 绿色icon弹出框 -->
-		<div class="dialog-icon-success">
-			<div class="icon-bor"><i class="icon">&#xe60b;</i></div>
-			<div class="title">
+		<div class="KsDialogIcon KsDialogIcon--success">
+			<div class="KsDialogIcon__icon-bor"><i class="icon">&#xe60b;</i></div>
+			<div class="KsDialogIcon__title">
 				<h2>标题</h2>
 			</div>
-			<div class="content">
+			<div class="KsDialogIcon__content">
 				警告信息！警告信息！警告信息！
 			</div>
-			<div class="operation">
+			<div class="KsDialogIcon__operation">
 				<button class="btn-plain-dark">取消</button>
 				<button class="btn-success">确定</button>
 			</div>
 		</div>
+
+		<pre>
+			<code class="html">
+				<!-- 红色icon弹出框 -->
+				<div class="KsDialogIcon KsDialogIcon--danger">
+					<div class="KsDialogIcon__icon-bor"><i class="icon">&#xe60d;</i></div>
+					<div class="KsDialogIcon__title">
+						<h2>标题</h2>
+					</div>
+					<div class="KsDialogIcon__content">
+						警告信息！警告信息！警告信息！
+					</div>
+					<div class="KsDialogIcon__operation">
+						<button class="btn-plain-dark">取消</button>
+						<button class="btn-danger">确定</button>
+					</div>
+				</div>
+				<!-- 青色icon弹出框 -->
+				<div class="KsDialogIcon KsDialogIcon--info">
+					<div class="KsDialogIcon__icon-bor"><i class="icon">&#xe61b;</i></div>
+					<div class="KsDialogIcon__title">
+						<h2>标题</h2>
+					</div>
+					<div class="KsDialogIcon__content">
+						警告信息！警告信息！警告信息！
+					</div>
+					<div class="KsDialogIcon__operation">
+						<button class="btn-plain-dark">取消</button>
+						<button class="btn-info">确定</button>
+					</div>
+				</div>
+				<!-- 橙色icon弹出框 -->
+				<div class="KsDialogIcon KsDialogIcon--warning">
+					<div class="KsDialogIcon__icon-bor"><i class="icon">&#xe60d;</i></div>
+					<div class="KsDialogIcon__title">
+						<h2>标题</h2>
+					</div>
+					<div class="KsDialogIcon__content">
+						警告信息！警告信息！警告信息！
+					</div>
+					<div class="KsDialogIcon__operation">
+						<button class="btn-plain-dark">取消</button>
+						<button class="btn-warning">确定</button>
+					</div>
+				</div>
+
+				<!-- 绿色icon弹出框 -->
+				<div class="KsDialogIcon KsDialogIcon--success">
+					<div class="KsDialogIcon__icon-bor"><i class="icon">&#xe60b;</i></div>
+					<div class="KsDialogIcon__title">
+						<h2>标题</h2>
+					</div>
+					<div class="KsDialogIcon__content">
+						警告信息！警告信息！警告信息！
+					</div>
+					<div class="KsDialogIcon__operation">
+						<button class="btn-plain-dark">取消</button>
+						<button class="btn-success">确定</button>
+					</div>
+				</div>
+			</code>
+		</pre>
 	</div>
 </template>
--- dev/js/views/select/select.vue
+++ dev/js/views/select/select.vue
@@ -2,72 +2,55 @@
 	<div>
 
 		<!-- 下拉列表 -->
-		<div class="select" style="margin-bottom: 265px;">
-			<div class="select-hd">
+		<div class="KsSelect" style="margin-bottom: 265px; margin-left: 100px;">
+			<div class="KsSelect__hd">
 				<div class="col">选择内容</div>
 				<i class="icon col-auto">&#xe619;</i>
 			</div>
-			<div class="select-bd">
-				<div class="select-search">
-					<div class="row-auto">
+			<div class="KsSelect__bd">
+				<div class="KsSelect__search">
+					<div class="KsSelect__search__input row-auto">
 						<i class="icon col-auto">&#xe617;</i>
 						<input type="text">
 					</div>
 				</div>
-				<ul class="select-list">
-					<li>选择内容</li>
-					<li class="active">选择内容选择内容</li>
-					<li>选择内容</li>
-					<li>选择内容</li>
-					<li>选择内容</li>
-					<li>选择内容</li>
-					<li>选择内容</li>
-					<li>选择内容</li>
-					<li>选择内容</li>
-					<li>选择内容</li>
-					<li>选择内容</li>
+				<ul class="KsSelect__list">
+					<li class="KsSelect__list__li">选择内容</li>
+					<li class="KsSelect__list__li KsSelect__list__active">选择内容选择内容</li>
+					<li class="KsSelect__list__li">选择内容</li>
+					<li class="KsSelect__list__li">选择内容</li>
+					<li class="KsSelect__list__li">选择内容</li>
+					<li class="KsSelect__list__li">选择内容</li>
+					<li class="KsSelect__list__li">选择内容</li>
+					<li class="KsSelect__list__li">选择内容</li>
+					<li class="KsSelect__list__li">选择内容</li>
+					<li class="KsSelect__list__li">选择内容</li>
+					<li class="KsSelect__list__li">选择内容</li>
 				</ul>
 			</div>
 		</div>
 
 		<pre>
         <code class="html">
-		<div class="select">
-			<div class="select-hd">
+		<div class="KsSelect" style="margin-bottom: 265px; margin-left: 100px;">
+			<div class="KsSelect__hd">
 				<div class="col">选择内容</div>
 				<i class="icon col-auto">&#xe619;</i>
 			</div>
-			<div class="select-bd">
-				<div class="select-search">
-					<div class="row-auto">
+			<div class="KsSelect__bd">
+				<div class="KsSelect__search">
+					<div class="KsSelect__search__input row-auto">
 						<i class="icon col-auto">&#xe617;</i>
 						<input type="text">
 					</div>
 				</div>
-				<ul class="select-list">
-					<li>选择内容</li>
-					<li class="active">选择内容选择内容</li>
+				<ul class="KsSelect__list">
+					<li class="KsSelect__list__li">选择内容</li>
+					<li class="KsSelect__list__li KsSelect__list__active">选择内容选择内容</li>
+					<li class="KsSelect__list__li">选择内容</li>
 				</ul>
 			</div>
 		</div>
-        </code>
-        </pre>
-
-        <pre>
-        <code class="scss">
-		// 下拉列表组件
-		.select{
-			// ......
-			.select-hd{...}  //显示选中内容区
-			.select-bd{
-				// ......
-				.select-list{  // 下拉列表内容
-					// ......
-					.active,.active:hover{...} //下拉区中选中样式
-				}
-				.search{...}  // 搜索框
-			}
-		}
         </code>
         </pre>
 
