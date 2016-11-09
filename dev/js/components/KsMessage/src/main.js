c884950b7c4ccc38050ce3a05b04de8fdcd8f1e7
pkeros <pkeros@vip.qq.com>
2016-11-09 15:35:23 +0800

    added `KsMessage` component.

---
+++ dev/js/components/KsMessage/src/main.js
@@ -0,0 +1,183 @@
+/**
+ * @description Dialog 对话框
+ * @summary
+ *  可以进行全局调用的哦, 棒棒哒
+ * @author: pkeros.
+ * @date: 2016/10/19.
+ */
+
+import Vue from 'vue'
+import Dialog from './main.vue'
+import KsMask from '../../KsMask'
+
+const pueMapper = ['success', 'info', 'warn', 'danger']
+let defaults = {
+  showCancelBtn: false,
+  cancelBtnText: '取消',
+  confirmBtnText: '确定',
+  mask: true,
+  title: 'Title',
+  text: 'Content...',
+  type: 'success'
+}
+
+let KsDialogConstructor = Vue.extend(Dialog)
+
+let currentMsg, instance, KsDialog, id = 0
+let DialogQueue = []
+
+/**
+ * @description 合并选项
+ * @param target 需要合并的目标
+ * @return {*} 目标
+ */
+let merge = function(target) {
+  for (let i = 1, j = arguments.length; i < j; i++) {
+    let source = arguments[i]
+    for (let prop in source) {
+      if (source.hasOwnProperty(prop)) {
+        let value = source[prop]
+        if (value !== undefined) {
+          target[prop] = value
+        }
+      }
+    }
+  }
+
+  return target;
+}
+
+/**
+ * @description 初始化 Dialog 实例
+ */
+let initInstance = function () {
+  // 实例化 Dialog
+  instance = new KsDialogConstructor({
+    el: document.createElement('div')
+  })
+  instance.show = false
+
+  // 监听关闭动作
+  instance.$on('close', KsDialog.close)
+}
+
+/**
+ * @description 显示队列中的下一个信息
+ */
+let showNextDialog = function () {
+  if (!instance) {
+    initInstance()
+  }
+
+  // 检测是否阻塞
+  if (instance.show || currentMsg || !DialogQueue.length) {
+    return
+  }
+
+  // 获取当前信息
+  currentMsg = DialogQueue.shift()
+
+  // 合并配置项
+  let options = currentMsg.options
+  for (let prop in options) {
+    if (instance.hasOwnProperty(prop)) {
+      instance[prop] = options[prop]
+    }
+  }
+
+  // 绑定事件
+  let {
+    confirmCb = KsDialog.close,
+    cancelCb = KsDialog.close
+  } = currentMsg
+  instance.$off('confirm')
+  instance.$off('cancel')
+  instance.$on('confirm', confirmCb)
+  instance.$on('cancel', cancelCb)
+
+  // 实例化 mask
+  if (typeof currentMsg.maskInstance !== 'undefined') {
+    instance['maskConfig'] = currentMsg.maskInstance(cancelCb)
+  }
+
+  document.body.appendChild(instance.$el)
+  Vue.nextTick(() => instance.show = true)
+}
+
+/**
+ * @description 构造函数
+ * @param options {Object} 配置项
+ * @constructor
+ */
+KsDialog = function (options) {
+  // 配置 Dialog 并加入显示队列
+  return function (confirmCb, cancelCb) {
+    // 参数正确性校验
+    if ((typeof confirmCb !== 'undefined' && typeof confirmCb !== 'function')
+    || (typeof cancelCb !== 'undefined' && typeof cancelCb !== 'function')) {
+      throw new TypeError('KsDialog: Parameter is not correct, member not a function!')
+    }
+
+    let config = {
+      id: id++,
+      options: merge({}, defaults, KsDialog.defaults || {}, options),
+      confirmCb: confirmCb,
+      cancelCb: cancelCb,
+    }
+
+    // 创建 mask 配置
+    options.mask && (config['maskInstance'] = KsMask(options))
+
+    DialogQueue.push(config)
+    showNextDialog()
+    return config
+  }
+}
+
+/**
+ * @description 关闭 Dialog
+ */
+KsDialog.close = function () {
+  instance.show = false
+  currentMsg = null
+
+  showNextDialog()
+}
+
+/**
+ * @description dialog
+ * @param text {String} 显示的内容
+ * @param title {String} 标题
+ * @param hue {String} 色调
+ * @param options {Object} 附加配置项
+ */
+KsDialog.dialog = function (text, title, hue, options) {
+  return KsDialog(merge({
+    showConfirmBtn: true,
+    showCancelBtn: true,
+    text: text,
+    title: title,
+    mask: true,
+    type: hue
+  }, options))
+}
+
+/**
+ * @description 设置默认配置项
+ * @param defaults 默认配置项
+ */
+KsDialog.setDefaults = function (defaults) {
+  KsDialog.defaults = defaults
+}
+
+// 注册不同色调的函数
+pueMapper.forEach(hue => {
+  KsDialog[hue] = function (text, title, cancel = false) {
+    return KsDialog.dialog(text, title, hue, {
+      showCancelBtn: cancel
+    })
+  }
+})
+
+export default KsDialog
+export { KsDialog }
---
+++ dev/js/components/KsMessage/src/main.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="KSDialog__wrapper">
+    <!-- 不想加这个 wrapper, but 不加会变成片断实例 -->
+    <div :class="'dialog-icon KSDialog KSDialog__UID--' + _uid"
+         v-if="show" transition="Zoom" :style="zIndex">
+      <!--
+        TODO: 需要设置模态框的背景颜色, 不然遮罩时候背景是透明的, 还需要设置定位, 使 z-index 生效,
+        TODO: 组件要是圆角这点也需要处理下
+        TODO: 居中效果也是需要添加
+      -->
+      <style type="text/css">
+        .KSDialog {
+          position: relative; background: #FFF;
+        }
+      </style>
+      <!-- 模态颜色 -->
+      <style type="text/css">
+        .KSDialog__UID--{{ _uid }} .icon-bor {
+          color: {{ hue.hue }};
+        }
+      </style>
+      <!-- TODO: 此处 class 需要注意修改 -->
+      <aside class="KSDialog__icon icon-bor">
+        <i :class="'icon ' + hue.icon"></i>
+      </aside>
+      <article class="KSDialog__content">
+        <!--
+          TODO: 此处 class 需要注意修改
+          TODO: 这边一个小小的 title 也会定义这么长的 class 是想强调它的结构性
+         -->
+        <h3 class="KSDialog__title title">
+          {{ title }} <slot name="title"></slot>
+        </h3>
+        <!-- TODO: 此处 class 需要注意修改 -->
+        <p class="content">
+          {{ text }} <slot name="text"></slot>
+        </p>
+      </article>
+      <!-- TODO: 此处 class 需要注意修改 -->
+      <footer class="KSDialog__footer operation">
+        <aside class="KSDialog__btnWarp">
+          <ks-button :ghost="true" type="other" @click.stop="$emit('cancel')"
+                     v-if="showCancelBtn" style="margin-right: 10px"
+          >{{ cancelBtnText }}</ks-button>
+          <ks-button :type="type" @click.stop="$emit('confirm')"
+          >{{ confirmBtnText }}</ks-button>
+        </aside>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+  import KsButton from '../../KsButton'
+  import KsMask from '../../KsMask'
+
+  // 类型对色调映射
+  const colorMapper = {
+    success: { hue: '#4CAF50', icon: 'icon-chenggongtubiao' },
+    info: { hue: '#00BCD4', icon: 'icon-xinxitubiao' },
+    warn: { hue: '#FF5722', icon: 'icon-cuowutubiao' },
+    danger: { hue: '#F44336', icon: 'icon-cuowutubiao' }
+  }
+  // z-index
+  let zIndex = 19941026
+
+  export default {
+    name: 'KSDialog',
+
+    data () {
+      return { }
+    },
+
+    props: {
+      showCancelBtn: { type: Boolean, default: true },
+      cancelBtnText: { type: String, default: '取消' },
+      confirmBtnText: { type: String, default: '确定' },
+      title: { type: String, default: '' },
+      text: { type: String, default: '' },
+      type: { type: String, default: 'success' },
+      mask: { type: Boolean, default: true },
+      show: { type: Boolean, default: true, towWay: true }
+    },
+
+    computed: {
+      /**
+       * @description 当前模态的主色调
+       * @return {*} color
+       */
+      hue () { return colorMapper[this.type] },
+      /**
+       * @description 用于控制组件的层叠顺序
+       * @return {string} z-index
+       */
+      zIndex () { return `z-index:${zIndex++}` }
+    },
+
+    watch: {
+      show (show) {
+        let maskConfig = this.maskConfig
+
+        if (!show && maskConfig) {
+          KsMask.close()
+        }
+      }
+    },
+
+    components: { KsButton, KsMask }
+  }
+</script>
+
+<style lang="scss">
+</style>
