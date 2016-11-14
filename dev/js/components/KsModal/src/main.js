96997b9b9bf9b38195fff9bc36c29e46d44dc0ee
pkeros <pkeros@vip.qq.com>
2016-11-14 15:41:27 +0800

    code style, trim.

--- dev/js/components/KsModal/src/main.js
+++ dev/js/components/KsModal/src/main.js
@@ -54,7 +54,6 @@ let merge = function(target) {
  */
 let initInstance = function () {
   // 实例化 modal
-  debugger
   instance = new KsModalConstructor({
     el: document.createElement('div')
   })
